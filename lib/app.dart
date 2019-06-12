1f075e30c8555147ed445e0a45987f6e4dced2a5
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-06-12 23:35:07 +0200

    Migrate to AndroidX and add notification lib

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,69 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:tgv_max_alert/main.dart';
+import 'package:tgv_max_alert/screens/add_alert_screen.dart';
 import 'package:tgv_max_alert/screens/home_screen.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  App({Key key}) : super(key: key);
+
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    var initAndroid = AndroidInitializationSettings('ic_notif_train');
+    var initIOS = IOSInitializationSettings(
+      onDidReceiveLocalNotification: onDidReceiveLocalNotification,
+    );
+    var initSettings = InitializationSettings(initAndroid, initIOS);
+    flutterLocalNotificationsPlugin.initialize(
+      initSettings,
+      onSelectNotification: onSelectNotification,
+    );
+  }
+
+  Future<void> onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+            title: Text(title),
+            content: Text(body),
+            actions: [
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text('Ok'),
+                onPressed: () async {
+                  Navigator.of(context, rootNavigator: true).pop();
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AddAlertScreen(),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+    );
+  }
+
+  Future<void> onSelectNotification(String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AddAlertScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tgv_max_alert/app.dart';
 import 'package:tgv_max_alert/utils/preferences.dart';
 
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
 void main() async {
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  
   initializeDateFormatting("fr_FR");
 
   Preferences.sharedPreferences = await SharedPreferences.getInstance();
